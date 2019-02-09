b1ec785f815534c2130f6d19653cf5f8d6cc7251
gurminder <kaurdeol99@gmail.com>
2019-02-08 19:35:37 -0500

    array slicing

--- sampleday2/main.swift
+++ sampleday2/main.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+/*
 print("Hello, World!")
 var a:Int=60
 /*
@@ -56,3 +56,43 @@ if(q.contains(-1000))
 {
     print("TRue")
 }
+*/
+//array
+var a = [10,20,30,40,50]
+for i in a
+{
+    print(i)
+}
+print(a.count)
+print(a[0])
+//these three wont work
+//print(a[5])
+//a[5]==1000
+//print(a[5])print(c[1]
+var b=[Int]()
+b.append(100)
+b.append(200)
+b.append(300)
+print(b[0],b[1],b[2])
+b += [111,222,333]
+print(b)
+let c = a + b
+for i in c
+{
+    print(i)
+}
+print("values of c[1..3]array")
+var x=c[1...3]
+print(x)
+x[1]=2020
+for i in x
+{
+    print(i)
+}
+print("value of c[1] array")
+print(c[1])
+var q = c [2...10]
+print(q[2])
+var z = Array<Int>()
+z.append(5000)
+print(z)
